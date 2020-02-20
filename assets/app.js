2f87dfba8de5d9616e152436524fa1926ed6f924
Anon3m!ty <60500112+Anon3mity@users.noreply.github.com>
2020-02-20 06:13:02 +0530

    js

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+<script>
+ src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+ src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js">
+  data-ad-client="ca-pub-7926875234119715" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
+
+ </script>
