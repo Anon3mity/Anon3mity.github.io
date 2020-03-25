1f5f9dbdeb5f918b6fd3a6a4aef09cb8f0896d8b
Anon3mity <60500112+Anon3mity@users.noreply.github.com>
2020-03-25 09:33:31 +0530

    xc

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
-// sw.js
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
     prefix: 'anon3mity.github.io',
@@ -18,7 +17,7 @@ workbox.routing.setDefaultHandler(workbox.strategies.networkFirst());
 
 // let Workbox handle our precache list
 // NOTE: This will be populated by jekyll-workbox-plugin.
-workbox.precaching.precacheAndRoute([{"url":"/index.html","revision":"1489bb60f328dab1ce30cd40a5af1b47"},{"url":"/android/ui/2020/02/07/Shape-Drawable-Button.Xml-Android-Studio/","revision":"86513c5d448727ad543a11fa4d96879e"}]);
+workbox.precaching.precacheAndRoute([{"url":"/index.html","revision":"a041a94d81c5e69c9e89bbcfd906138e"},{"url":"/android/ui/2020/02/07/Shape-Drawable-Button.Xml-Android-Studio/","revision":"86513c5d448727ad543a11fa4d96879e"}]);
 
 // use `Stale-while-revalidate` strategy for images and fonts.
 workbox.routing.registerRoute(
