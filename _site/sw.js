44b7d12520312c49a2b454ef82e7e24fefc94ac0
Anon3mity <60500112+Anon3mity@users.noreply.github.com>
2020-03-25 13:11:42 +0530

    go

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,31 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+// sw.js
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+// set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
     prefix: 'anon3mity.github.io',
     suffix: 'v1',
     precache: 'precache',
     runtime: 'runtime-cache'
 });
+
+// let Service Worker take control of pages ASAP
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+// default to `networkFirst` strategy
+workbox.routing.setDefaultHandler(workbox.strategies.networkFirst());
+
 // let Workbox handle our precache list
 // NOTE: This will be populated by jekyll-workbox-plugin.
 workbox.precaching.precacheAndRoute([{"url":"/index.html","revision":"fd89643f80b5b1711578679421f0d77b"},{"url":"/android/ui/2020/02/07/Shape-Drawable-Button.Xml-Android-Studio/","revision":"86513c5d448727ad543a11fa4d96879e"}]);
+
+// use `Stale-while-revalidate` strategy for images and fonts.
+workbox.routing.registerRoute(
+    /images/,
+    workbox.strategies.staleWhileRevalidate()
+);
+workbox.routing.registerRoute(
+    /^https?:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate()
+);
