a7be6de31894cfa339d979a5f8e4415d60d08000
Anon3mity <dev.anon3mity@gmail.com>
2020-04-04 00:45:46 +0530

    ch

--- _site/sw.js
+++ _site/sw.js
@@ -18,7 +18,7 @@ workbox.routing.setDefaultHandler(workbox.strategies.networkFirst());
 
 // let Workbox handle our precache list
 // NOTE: This will be populated by jekyll-workbox-plugin.
-workbox.precaching.precacheAndRoute([{"url":"/index.html","revision":"23d3aaa948df3282f37a9c393110ab7c"},{"url":"/android/ui/2020/02/07/Shape-Drawable-Button.Xml-Android-Studio-copy-2/","revision":"86513c5d448727ad543a11fa4d96879e"}]);
+workbox.precaching.precacheAndRoute([{"url":"/index.html","revision":"ec5e2124a8b1bd674d33263c3db7d198"},{"url":"/android/ui/2020/02/07/Shape-Drawable-Button.Xml-Android-Studio-copy-2/","revision":"86513c5d448727ad543a11fa4d96879e"}]);
 
 // use `Stale-while-revalidate` strategy for images and fonts.
 workbox.routing.registerRoute(
