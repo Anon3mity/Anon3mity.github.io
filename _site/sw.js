5296a3bafafc1a8867a30227cfa724a1f66d664d
Anon3mity <dev.anon3mity@gmail.com>
2020-04-23 17:48:10 +0530

    setup

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 // sw.js
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
     prefix: 'anon3mity.github.io',
@@ -18,7 +18,7 @@ workbox.routing.setDefaultHandler(workbox.strategies.networkFirst());
 
 // let Workbox handle our precache list
 // NOTE: This will be populated by jekyll-workbox-plugin.
-workbox.precaching.precacheAndRoute([{"url":"/index.html","revision":"85e5a2dc5f414408d2c51864ba50b352"},{"url":"/2020/02/07/Learn-smarter-&-Better/","revision":"e6bbfc99e19a9da1fe2b18ec5d4675c4"}]);
+workbox.precaching.precacheAndRoute([{"url":"/index.html","revision":"02bcc2ff3009908af90356ee4dd312db"},{"url":"/about/","revision":"02bcc2ff3009908af90356ee4dd312db"},{"url":"/blogs/","revision":"02bcc2ff3009908af90356ee4dd312db"},{"url":"/contact/","revision":"02bcc2ff3009908af90356ee4dd312db"},{"url":"/courses/","revision":"02bcc2ff3009908af90356ee4dd312db"},{"url":"/privacy/","revision":"02bcc2ff3009908af90356ee4dd312db"},{"url":"/products/","revision":"02bcc2ff3009908af90356ee4dd312db"},{"url":"/videos/","revision":"02bcc2ff3009908af90356ee4dd312db"},{"url":"/2020/02/07/Learn-smarter-&-Better/","revision":"e6bbfc99e19a9da1fe2b18ec5d4675c4"}]);
 
 // use `Stale-while-revalidate` strategy for images and fonts.
 workbox.routing.registerRoute(
---
+++ _videos/custom_theme.md
@@ -0,0 +1,9 @@
+---
+title: GitHub Pages Custom Jekyll Theme
+img: "/assets/img/anon.jpg"
+layout: youtube
+youtubeId: Uji_txi-xHw
+res: "https://github.com/137unknown/custom_jekyll_theme"
+---
+
+{% include videos.html id=page.youtubeId %}
